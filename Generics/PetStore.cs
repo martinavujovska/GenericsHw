8afbdf902cbc79367228aa3f0d6e02489b1f76f3
Acer <Acer@DESKTOP-NE8MC2B>
2022-05-06 19:47:57 +0200

    Add project files.

---
+++ Generics/PetStore.cs
@@ -0,0 +1,35 @@
+﻿using GenericsHomework.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    public class PetStore<T> where T : Pet
+    {
+        public List<T> pets = new List<T>();
+
+        public void PrintPets()
+        {
+            foreach (T pet in pets)
+            {
+                Console.WriteLine(pet.PrintInfo());
+            }
+        }
+        public void BuyPet(string name)
+        {
+            T pet = pets.Single(x => x.Name == name);
+            if (pet == null)
+            {
+                Console.WriteLine($"We don't have that animal, sry. ");
+
+            }
+            pets.Remove(pet);
+            Console.WriteLine($"You have successfully bought {pet.Name}.");
+        }
+    }
+}
+
+
---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using Generics.Classes;
+using GenericsHomework;
+using GenericsHomework.Classes;
+using GenericsHomework.Enums;
+using GenericsHomework.Classes;
+
+Cat Tosho = new Cat("Tosho", 1, GenericsHomework.Enums.Type.Cat, 9, true);
+Cat Ganymede = new Cat("Ganymede", 11, GenericsHomework.Enums.Type.Cat, 7, false);
+
+Dog Nela = new Dog("Nela", 7, GenericsHomework.Enums.Type.Dog, true, "Dog food");
+Dog Oskar = new Dog("Oskar", 5, GenericsHomework.Enums.Type.Dog, false, "Meat");
+
+Fish Orange = new Fish("Orange", 2, GenericsHomework.Enums.Type.Fish, "Gold", "large");
+Fish Bobby = new Fish("Bobby", 10, GenericsHomework.Enums.Type.Fish, "Grey", "extra large");
+
+
+PetStore<Fish> FishStore = new PetStore<Fish>();
+PetStore<Dog> DogStore = new PetStore<Dog>();
+PetStore<Cat> CatStore = new PetStore<Cat>();
+PetStore<Pet> WholeStore = new PetStore<Pet>();
+
+List<Fish> FishList = new List<Fish> { Orange, Bobby };
+List<Dog> DogList = new List<Dog> { Nela, Oskar };
+List<Cat> CatList = new List<Cat> { Tosho, Ganymede };
+List<Pet> PetList = new List<Pet> { Tosho, Ganymede, Nela, Oskar, Orange, Bobby };
+
+FishStore.pets.Add(Orange);
+FishStore.pets.Add(Bobby);
+DogStore.pets.Add(Nela);
+DogStore.pets.Add(Oskar);
+CatStore.pets.Add(Tosho);
+CatStore.pets.Add(Ganymede);
+
+Console.WriteLine("Dogs:");
+DogStore.PrintPets();
+Console.WriteLine("Cats:");
+CatStore.PrintPets();
+Console.WriteLine("Fish:");
+FishStore.PrintPets();
+
+
+
+DogStore.BuyPet(Oskar.Name);
+CatStore.BuyPet(Ganymede.Name);
+
+
+
